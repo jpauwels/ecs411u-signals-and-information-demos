e99019ed0071fb0a7260db3b5626a966a954becf
Johan Pauwels <jpauwels@users.noreply.github.com>
2026-03-22 21:09:24 +0000

    Update AM pages with CDN, updated AM plot and improved AM variants

--- modulation-schemes.js
+++ modulation-schemes.js
@@ -5,3 +5,35 @@ function sineWave(timeStamps, amp, freq) {
 function cosineWave(timeStamps, amp, freq) {
     return timeStamps.map((t) => amp * Math.cos(2*Math.PI*freq*t));
 }
+
+function modulateDsbAm(msgSamples, carrSamples, ampCarr) {
+    const shiftedMsgSamples = msgSamples.map((v) => Math.max(0, 1 + v / ampCarr));
+    return shiftedMsgSamples.map((msg, idx) => msg * carrSamples[idx]);
+}
+
+function modulateDsbScAm(msgSamples, carrSamples, ampCarr) {
+    return msgSamples.map((sample, idx) => sample * carrSamples[idx] / ampCarr);
+}
+
+function modulateSsbScAm(msgSamples, carrSamples, ampCarr) {
+	const hilbertMsg = fourier.hilbert(msgSamples).hilbertImag;
+	const hilbertCarr = fourier.hilbert(carrSamples).hilbertImag
+    return msgSamples.map((sample, idx) => (sample * carrSamples[idx] - hilbertMsg[idx] * hilbertCarr[idx]) / (2*ampCarr));
+}
+
+function amplitudeModulation(samples, ampCarr, freqCarr, samplerate) {
+    const timeStamps = samples.map((_, idx) => idx / samplerate);
+    const carrSamples = cosineWave(timeStamps, ampCarr, freqCarr);
+    return modulateDsbScAm(samples, carrSamples, ampCarr);
+}
+
+function dsbScModulation(samples, ampCarr, freqCarr, samplerate) {
+	const timeStamps = samples.map((_, idx) => idx / samplerate);
+    const carrSamples = cosineWave(timeStamps, ampCarr, freqCarr);
+    return modulateDsbScAm(samples, carrSamples)
+}
+
+function amplitudeDemodulation(samples, ampCarr) {
+	const envSamples = fourier.envelope(samples);
+    return envSamples.map((t) => t - ampCarr);
+}
